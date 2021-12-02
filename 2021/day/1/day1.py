d616f48d851f9bcf475c13abeb14faa3c23dba4e
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-12-02 10:26:56 -0800

    Advent of Code: Day 1 (#3)
    * update: gitignore.
    
    * add: day1.
    
    * add: code for determining larger measurements.
    
    * add: print statement for input into advent of code.
    
    * add: test data.
    
    * fix: index off by 1.
    
    * change: prep for part2.
    
    * fix: int instead of str (thanks Adam!)
    
    * add: part2 code.
    
    * change: formatting.

---
+++ 2021/day/1/day1.py
@@ -0,0 +1,54 @@
+"""
+Day 1
+"""
+
+
+def difference(measures):
+    return sum(
+        second > first
+        for first, second
+        in zip(
+            measures[:-1],
+            measures[1:]
+        )
+    )
+
+
+def sum_three(measures):
+    return [
+        first + second + third
+        for first, second, third
+        in zip(
+            measures[:-2],
+            measures[1:-1],
+            measures[2:]
+        )
+    ]
+
+
+def part1(measures):
+    """
+    How many measurements are larger than the previous measurement?
+    :param measures: Data to be differed.
+    :return: Number of measurements larger than previous
+    """
+    return difference(measures)
+
+
+def part2(measures):
+    """
+    How many sums are larger than the previous sum?
+    :param measures: Data to be differed.
+    :return: Number of sums larger than previous
+    """
+    return difference(sum_three(measures))
+
+
+if __name__ == '__main__':
+    # Load txt file to list
+    with open("input.txt", "r") as file:
+        measurements = [int(element) for element in file.read().splitlines()]
+    # Part 1
+    print(part1(measurements))
+    # Part 2
+    print(part2(measurements))
