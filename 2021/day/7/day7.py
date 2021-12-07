e3a970eb27f331b2bceb7d87d5cbeceb29484275
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-12-06 22:04:34 -0800

    add: day7 part2.

--- 2021/day/7/day7.py
+++ 2021/day/7/day7.py
@@ -7,7 +7,8 @@ def main():
     with open("input.txt", "r") as file:
         crabs = file.read().strip().split(',')
     crabs = [int(crab) for crab in crabs]
-    print(min(sum(abs(crab - distance) for distance in crabs) for crab in crabs))
+    print(min(sum(abs(crab - distance) for distance in crabs) for crab in crabs))  # Part 1
+    print(min(sum(sum(range(abs(crab - distance) + 1)) for crab in crabs) for distance in range(max(crabs))))  # Part 2
 
 
 if __name__ == '__main__':
