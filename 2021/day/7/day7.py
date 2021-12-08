381151c647ba6b8c91ac8e4ac021e8175c4a1041
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-12-08 00:35:37 -0800

    Advent of Code: Day 8 (#19)
    * add: day8 part1.
    
    * change: day8 part2.
    
    * 'Refactored by Sourcery' (#20)
    
    Co-authored-by: Sourcery AI <>
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- 2021/day/7/day7.py
+++ 2021/day/7/day7.py
@@ -7,7 +7,7 @@ def main():
     with open("input.txt", "r") as file:
         crabs = file.read().strip().split(',')
     crabs = [int(crab) for crab in crabs]
-    print(min(sum(abs(crab - distance) for distance in crabs) for crab in crabs))  # Part 1
+    print(min(sum(abs(crab - distance) for crab in crabs) for distance in range(max(crabs))))  # Part 1
     print(min(sum(sum(range(abs(crab - distance) + 1)) for crab in crabs) for distance in range(max(crabs))))  # Part 2
 
 
---
+++ 2021/day/8/day8.py
@@ -0,0 +1,51 @@
+"""
+Day 8
+"""
+import itertools
+
+
+class SevenSegment:
+
+    def __init__(self, line):
+        self.patterns, self.output = line.split('|')
+        self.patterns = [''.join(sorted(pattern)) for pattern in self.patterns.split(' ') if pattern]
+        self.digits = self.determine_digits()
+        self.output = [''.join(sorted(output)) for output in self.output.split(' ') if output]
+
+    def determine_digits(self):
+        for key in [''.join(p) for p in itertools.permutations('abcdefg')]:
+            digits = {
+                0: f'{key[:3]}{key[4:7]}',
+                1: f'{key[2]}{key[5]}',
+                2: f'{key[0]}{key[2:5]}{key[6]}',
+                3: f'{key[0]}{key[2:4]}{key[5:7]}',
+                4: f'{key[1:4]}{key[5]}',
+                5: f'{key[:2]}{key[3]}{key[5:7]}',
+                6: f'{key[:2]}{key[3:7]}',
+                7: f'{key[0]}{key[2]}{key[5]}',
+                8: f'{key}',
+                9: f'{key[:4]}{key[5:7]}',
+            }
+
+            if sorted([''.join(sorted(pattern)) for pattern in digits.values()]) == sorted(self.patterns):
+                return digits
+
+    def sum_easy_digits(self):
+        return sum(len(output) in [2, 4, 3, 7] for output in self.output)
+
+    def value_all_outputs(self):
+        output_value = {''.join(sorted(v)): str(k) for k, v in self.digits.items()}
+        return int(
+            f'{output_value[self.output[0]]}{output_value[self.output[1]]}{output_value[self.output[2]]}{output_value[self.output[3]]}'
+        )
+
+
+def main():
+    with open("input.txt", "r") as file:
+        seven_segments = [SevenSegment(line) for line in file.read().splitlines()]
+    print(sum(segment.sum_easy_digits() for segment in seven_segments))  # Part 1
+    print(sum(segment.value_all_outputs() for segment in seven_segments))  # Part 2
+
+
+if __name__ == '__main__':
+    main()
