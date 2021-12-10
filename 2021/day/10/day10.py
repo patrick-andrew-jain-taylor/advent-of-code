9afe6ee20c1642e7986e7141553cd86c45fc2458
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-12-10 15:46:14 -0500

    change: day10 part2.

--- 2021/day/10/day10.py
+++ 2021/day/10/day10.py
@@ -2,8 +2,9 @@
 Day 10
 """
 from collections import namedtuple
+import statistics
 
-OpenClose = namedtuple('OpenClose', 'open_chunk close_chunk value')
+CloseChunk = namedtuple('CloseChunk', 'illegal completion')
 
 open_chunk = {
     '(': ')',
@@ -13,29 +14,36 @@ open_chunk = {
 }
 
 close_chunk = {
-    ')': 3,
-    ']': 57,
-    '}': 1197,
-    '>': 25137
+    ')': CloseChunk(3, 1),
+    ']': CloseChunk(57, 2),
+    '}': CloseChunk(1197, 3),
+    '>': CloseChunk(25137, 4)
 }
 
 
 def chunk_check(line):
     prev_chunks = ''
     for chunk in line:
-        # print(prev_chunks)
         if chunk in '([{<':
             prev_chunks += chunk
         elif chunk in ')]}>':
             if chunk != open_chunk[prev_chunks[-1]]:
-                return close_chunk[chunk]
+                return CloseChunk(close_chunk[chunk].illegal, 0)
             prev_chunks = prev_chunks[:-1]
-    return 0
+    complete_chunk = [open_chunk[prev] for prev in prev_chunks]
+    completion = 0
+    for chunk in reversed(complete_chunk):
+        completion *= 5
+        completion += close_chunk[chunk].completion
+    return CloseChunk(0, completion)
 
 
 def main():
     with open("input.txt", "r") as file:
-        print(sum(chunk_check(line) for line in file.read().splitlines()))
+        lines = file.read().splitlines()
+    print(sum(chunk_check(line).illegal for line in lines))  # Part 1
+    print(statistics.median(
+        sorted(chunk_check(line).completion for line in lines if chunk_check(line).completion)))  # Part 2
 
 
 if __name__ == '__main__':
