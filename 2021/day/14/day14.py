f25300c5db2030208ec7699556e1b35f2cac9c84
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-12-17 16:42:08 -0500

    change: day14 part2 refactor attempt #1 (2^n complexity).

--- 2021/day/14/day14.py
+++ 2021/day/14/day14.py
@@ -8,7 +8,6 @@ class Polymer:
     def __init__(self, formula):
         self.template = formula[0]
         self.insertion = {k: v for rule in formula[2:] for k, v in dict([rule.split(' -> ')]).items()}
-        print(self.insertion)
 
     def __repr__(self):
         return self.template
@@ -16,26 +15,26 @@ class Polymer:
     def __str__(self):
         return self.template
 
-    def insert(self):
-        test_polymer = ''
-        for i in range(len(self.template) - 1):
-            pair = self.template[i:i + 2]
-            test_polymer += f'{self.template[i]}{self.insertion[pair]}'
-        test_polymer += self.template[-1]
-        self.template = test_polymer
-        return self
+    def _insert(self, polymer, times):
+        if not times:
+            return Counter(polymer[0])
+        times -= 1
+        return \
+            self._insert(f'{polymer[0]}{self.insertion[polymer]}', times) + \
+            self._insert(f'{self.insertion[polymer]}{polymer[1]}', times)
 
-    def difference(self):
-        most, *_, least = Counter(self.template).most_common()
-        return most[1] - least[1]
+    def insert(self, times=1):
+        count = Counter()
+        polymers = [self.template[i:i + 2] for i in range(len(self.template) - 1)]
+        for polymer in polymers:
+            count += self._insert(polymer, times)
+        return count + Counter(self.template[-1])
 
 
 def main():
-    with open("input.txt", "r") as file:
+    with open("test.txt", "r") as file:
         polymer = Polymer(list(file.read().splitlines()))
-        for _ in range(10):
-            polymer.insert()
-    print(polymer.difference())
+    polymer.insert(40)
 
 
 if __name__ == '__main__':
